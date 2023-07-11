94ddc1befc74ecda476d10f20eef4506e6a389ce
giustt <gustavodecassio00@gmail.com>
2023-07-11 17:12:29 -0300

    Adding Files

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+function changeText(element) {
+    element.innerHTML = "Envie Uma Mensagem via WhatsApp!";
+}
+
+function changeTextEmail(element) {
+    element.innerHTML = "Envie Uma Mensagem via Email!";
+}
+
+function restoreText(element) {
+    element.innerHTML = "+5511952658821";
+}
+
+function restoreTextEmail(element) {
+    element.innerHTML = "gustavodecassio00@gmail.com";
+}
