d63ecf5ae440b1d4767573d8d960216a1a86b9da
giustt <gustavodecassio00@gmail.com>
2023-07-14 17:02:55 -0300

    Api test icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,35 @@ function restoreTextEmail(element) {
     element.innerHTML = "gustavodecassio00@gmail.com";
 }
 
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+
+    const name = document.getElementById('profile.name')
+    name.innerHTML = profileData.name
+
+    const job = document.getElementById('profile.job')
+    job.innerHTML = profileData.job
+
+    const location = document.getElementById('profile.location')
+    location.innerHTML = profileData.location
+
+    const phone = document.getElementById('profile.phone')
+    phone.innerHTML = profileData.phone
+
+    const email = document.getElementById('profile.email')
+    email.innerHTML = profileData.email
+}
+
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById('profile.skills.hardSkills')
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
+}
+
 (async () => {
     const profileData = await fetchProfileData()
+    updateProfileInfo(profileData)
     console.log(profileData)
 })()
+
